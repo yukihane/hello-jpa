a308bde7614a0bc1201cb815f382081e4d66899d
yukihane <yukihane.feather@gmail.com>
2015-12-22 00:45:55 +0900

    問題になっているクラス

---
+++ src/main/java/com/github/yukihane/hello/java/jpa/domain/Child.java
@@ -0,0 +1,37 @@
+package com.github.yukihane.hello.java.jpa.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "child_table")
+@Entity
+public class Child implements Serializable {
+    private static final long serialVersionUID = 1718966005562173513L;
+    @EmbeddedId
+    private ChildKey childKey = new ChildKey();
+    @Column(name = "child_name")
+    private String childName;
+    @ManyToOne
+    @JoinColumn(name = "join_id", referencedColumnName = "join_id")
+    private Parent parent;
+
+    /** getter,setter省略 **/
+
+    @Embeddable
+    public static class ChildKey implements Serializable {
+        private static final long serialVersionUID = -3848325403357861609L;
+        @Column(name = "child_id")
+        String childId;
+        @Column(name = "join_id")
+        String joinId;
+    }
+
+    /** equals,hashCode省略 **/
+}
---
+++ src/main/java/com/github/yukihane/hello/java/jpa/domain/Parent.java
@@ -0,0 +1,43 @@
+package com.github.yukihane.hello.java.jpa.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Table(name = "parent_table")
+@Entity
+public class Parent implements Serializable {
+    private static final long serialVersionUID = 8058371942878604902L;
+
+    @EmbeddedId
+    private ParentKey parentKey = new ParentKey();
+
+    @Column(name = "parent_name")
+    private String parentName;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "join_id", referencedColumnName = "join_id")
+    private List<Child> childList;
+
+    /** getter,setter省略 **/
+
+    @Embeddable
+    public static class ParentKey implements Serializable {
+        private static final long serialVersionUID = -795173904285910114L;
+        @Column(name = "parent_id")
+        String parentId;
+        @Column(name = "join_id")
+        String joinId;
+    }
+
+    /** equals,hashCode省略 **/
+}
